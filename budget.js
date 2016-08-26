5fafbd6b0bf2c342c8bcdd68f3636f714ea6a23e
Gord Tanner <gord@bithound.io>
2016-08-26 08:32:04 -0400

    start up web stuff, borrowed initial structure from brooklynJS

---
+++ budget.js
@@ -0,0 +1,6 @@
+process.exit(
+     0.00 // budget.js - keeping the books for @corridor_js
+          // by @gordtanner and @alexkinsella
+
+					// Ideas borrowed with love from https://github.com/brooklynjs/brooklynjs.github.io
+);
