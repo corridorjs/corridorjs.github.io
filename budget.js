6f87f972fafac778781fad9868eeb7933902ac06
Gord Tanner <gord@dozr.com>
2016-09-30 16:02:49 -0400

    Budget for CorridorJS 0.0.1
    Surprise donation by Dozr to cover the bar tab means we can donate
    all of the ticket sales to kittens!
    
    Thanks to everyone who attended

--- budget.js
+++ budget.js
@@ -1,6 +1,15 @@
 process.exit(
-     0.00 // budget.js - keeping the books for @corridor_js
+     0.00 // budget.js - keeping the books for @corridorjs
           // by @gordtanner and @alexkinsella
 
           // Ideas borrowed with love from https://github.com/brooklynjs/brooklynjs.github.io
+     
+          // Corridor 0.0.1, 2016-09-29T19:00Z
+     
++  210.00 // ticket sales            @gtanner
++  146.98 // surprise sponsorship    @dozrhub
+
+-  146.98 // bar tab                 @mccabes
+-   17.44 // ticketing               @tito
+-  192.56 // donation                @kwhumane
 );
