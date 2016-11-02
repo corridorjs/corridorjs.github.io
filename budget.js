8fa06e27f87ce3b728936cb357a41417cf4e37ad
Gord Tanner <gord@dozr.com>
2016-11-02 09:55:54 -0400

    updated budget

--- budget.js
+++ budget.js
@@ -1,6 +1,6 @@
 process.exit(
      0.00 // budget.js - keeping the books for @corridorjs
-          // by @gordtanner and @alexkinsella
+          // by @gordtanner and @carlosperez
 
           // Ideas borrowed with love from https://github.com/brooklynjs/brooklynjs.github.io
      
@@ -12,4 +12,13 @@ process.exit(
 -  146.98 // bar tab                 @mccabes
 -   17.44 // ticketing               @tito
 -  192.56 // donation                @kwhumane
+
+          // Corridor 0.0.2, 2016-10-27T19:00Z
+     
++   70.00 // ticket sales            @gtanner
++   93.99 // sponsorship             @gtanner
+
+-   93.99 // bar tab                 @mccabes
+-   14.07 // ticketing               @tito
+-   55.93 // donation                @kwhumane
 );
